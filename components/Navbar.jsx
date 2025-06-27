ceff6f81aaac16671adb8d50895f90e4c67b8330
Cirooochen <cirochen0406@gmail.com>
2025-06-27 15:55:13 +0200

    add navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <a className="btn btn-ghost text-xl">B.W.M.F</a>
+    </div>
+  );
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import "./index.css";
+import { Navbar } from "../components/Navbar";
 
 const App = () => {
   const [text, setText] = useState("");
@@ -38,65 +39,68 @@ const App = () => {
   };
   const styles = darkMode ? "text-white" : "text-black";
   return (
-    <div
-      className={`mx-auto w-full h-[100vh] p-8 flex justify-center items-center
-         ${darkMode ? "bg-blue-700" : "bg-white"}`}
-    >
-      <div
-        className={`w-xl mx-auto 
-           p-6 shadow rounded ${darkMode ? "bg-white" : "bg-yellow-50"}`}
+    <body>
+      <Navbar />
+      <main
+        className={`mx-auto w-full h-[100vh] p-8 flex justify-center items-center
+           ${darkMode ? "bg-blue-700" : "bg-white"}`}
       >
-        <div className="flex flex-row justify-between">
-          <h1 className={`text-2xl font-bold mb-4 text-blue-700`}>
-            📝 Your Note
-          </h1>
-          <label>
-            <input
-              type="checkbox"
-              onChange={darkModeToggle}
-              className="w-4 h-4"
-            />
-            Dark Mode
-          </label>
-        </div>
-        <textarea
-          value={text}
-          onChange={handleChange}
-          className="w-full h-40 p-3 border rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-300"
-          placeholder="Type something interesting..."
-        ></textarea>
-        <div className="flex justify-between mt-2 text-sm text-gray-600">
-          <span>
-            Characters:
+        <div
+          className={`w-xl mx-auto
+             p-6 shadow rounded ${darkMode ? "bg-white" : "bg-yellow-50"}`}
+        >
+          <div className="flex flex-row justify-between">
+            <h1 className={`text-2xl font-bold mb-4 text-blue-700`}>
+              📝 Your Note
+            </h1>
+            <label>
+              <input
+                type="checkbox"
+                onChange={darkModeToggle}
+                className="w-4 h-4"
+              />
+              Dark Mode
+            </label>
+          </div>
+          <textarea
+            value={text}
+            onChange={handleChange}
+            className="w-full h-40 p-3 border rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-300"
+            placeholder="Type something interesting..."
+          ></textarea>
+          <div className="flex justify-between mt-2 text-sm text-gray-600">
+            <span>
+              Characters:
+              <span
+                className={`${
+                  text.length >= 200 ? "text-yellow-600" : "text-gray-600"
+                }`}
+              >
+                {text.length}
+              </span>
+              /{maxLength}
+            </span>
             <span
-              className={`${
-                text.length >= 200 ? "text-yellow-600" : "text-gray-600"
+              className={`font-medium ${
+                status === "Saved"
+                  ? "text-green-600"
+                  : status === "Cleared"
+                  ? "text-yellow-600"
+                  : "text-red-600"
               }`}
             >
-              {text.length}
+              {status}
             </span>
-            /{maxLength}
-          </span>
-          <span
-            className={`font-medium ${
-              status === "Saved"
-                ? "text-green-600"
-                : status === "Cleared"
-                ? "text-yellow-600"
-                : "text-red-600"
-            }`}
+          </div>
+          <button
+            onClick={clearNote}
+            className="mt-4 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded transition-all"
           >
-            {status}
-          </span>
+            Clear Note
+          </button>
         </div>
-        <button
-          onClick={clearNote}
-          className="mt-4 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded transition-all"
-        >
-          Clear Note
-        </button>
-      </div>
-    </div>
+      </main>
+    </body>
   );
 };
 
